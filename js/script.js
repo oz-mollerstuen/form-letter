4c6c608a20d0a1c5ef69c1dcf5c6d2033e9923fa
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-10-19 10:21:32 -0700

    add html, js
    Co-authored-by: Sonny Snyder <sendsonnymail@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 window.onload = function() {
     let form = document.querySelector("form");
     form.onsubmit = function(event) {
+      const name1 = document.getElementById("name1").value;
+      document.querySelector("span#name1a").innerText = name1;
+      document.querySelector("span#name1b").innerText = name1;
+      document.querySelector("div#letter").removeAttribute("class");
       event.preventDefault();
-      const someInput = document.getElementById("some-input").value;
-    };
   };
+};
